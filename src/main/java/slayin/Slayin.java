11891792301760da38067da60c2eea48a027bc70
Cristian Morbidelli <cristian.morbidelli@studio.unibo.it>
2024-03-28 11:07:29 +0100

    added time control between different frames

--- src/main/java/slayin/Slayin.java
+++ src/main/java/slayin/Slayin.java
@@ -3,18 +3,34 @@ package slayin;
 import slayin.core.Engine;
 
 public class Slayin {
+
+	static long tickTime = 25;	/* 40 fps */
+
     public static void main(String[] args) {
 		Engine core = new Engine();
+		long startTime, timePassed;
 		
 		while(core.stopLoop()){		/* Game loop */
+			startTime = System.currentTimeMillis();
+
 			/* TODO: check input */
 
 			/* TODO: update game status */
 
 			/* TODO: render updates */
+
+			timePassed = System.currentTimeMillis() - startTime;
+			if(timePassed < tickTime){	/* wait until tickTime before nextFrame */
+				try {
+					Thread.sleep(tickTime-timePassed);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			/*System.out.println(System.currentTimeMillis() - startTime);*/
 		}
 	}
-
 
 }
 
