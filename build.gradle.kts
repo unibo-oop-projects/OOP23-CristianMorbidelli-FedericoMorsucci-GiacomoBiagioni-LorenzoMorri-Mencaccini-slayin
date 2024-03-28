c0a80527799a3624a3c2e128a5a36139f48c7f04
Federico Morsucci <federico.morsucci@studio.unibo.it>
2024-03-28 11:57:07 +0100

    configured junit for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,16 @@
 plugins { java }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val junitVersion = "5.9.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/TempTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class TempTest {
+    
+
+    @Test
+    public void success(){
+        System.out.println("success");
+    }
+
+}
