3694d7a9ba7a8e651a47866868c93c1dce1bc4ab
Federico Morsucci <federico.morsucci@studio.unibo.it>
2024-03-29 09:15:57 +0100

    added pos inside GameObject

--- src/main/java/slayin/model/GameObject.java
+++ src/main/java/slayin/model/GameObject.java
@@ -1,5 +1,20 @@
 package slayin.model;
 
 abstract class GameObject {
+    private P2d pos;
+
+
+    public GameObject(P2d pos){
+        this.pos=pos;
+    }
+
+    public P2d getPos(){
+        return this.pos;
+    }
+
+    public P2d setPos(){
+        return this.pos;
+    }
+
 
 }
