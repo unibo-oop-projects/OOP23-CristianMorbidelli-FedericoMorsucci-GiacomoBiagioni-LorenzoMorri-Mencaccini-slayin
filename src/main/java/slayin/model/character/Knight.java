3cf986d177aab23652d62aab37b7d2a2a2d3b1ef
Federico Morsucci <federico.morsucci@studio.unibo.it>
2024-04-09 11:08:54 +0200

    first implementation of the jump and gravity for the knight, texts also written with a small graphic part to understand how it works

--- src/main/java/slayin/model/character/Knight.java
+++ src/main/java/slayin/model/character/Knight.java
@@ -8,28 +8,39 @@ import slayin.model.utility.Vector2d;
 
 public class Knight extends GameObject{
 
+    private Vector2d velocity,gravity;
+    private InputController lastInput;
+
     public Knight(P2d pos,Vector2d VectorMouvement,BoundingBox boundingBox) {
         super(pos,VectorMouvement,boundingBox);
+        velocity= new Vector2d(0, 0);
+        gravity= new Vector2d(0, +600);
     }
 
     @Override
     public void updateVel(InputController input) {
-        double speed= this.vectorMouvement.module();
+        lastInput=input;
         if(input.isMoveUp()){
-            this.setVectorMouvement(new Vector2d(0, 1).mul(speed));
-         }else if(input.isMoveDown()){
-            this.setVectorMouvement(new Vector2d(0,-1).mul(speed));
-         }else if(input.isMoveLeft()){
-            this.setVectorMouvement(new Vector2d(-1, 0).mul(speed));
-         }else if(input.isMoveRight()){
-            this.setVectorMouvement(new Vector2d(1, 0).mul(speed));
-         }
+            this.setVectorMouvement(new Vector2d(0, -4000));
+        }else if(input.isMoveLeft()){
+            this.setVectorMouvement(new Vector2d(-100, 0));
+        }else if(input.isMoveRight()){
+            this.setVectorMouvement(new Vector2d(100, 0));
+        }
     }
 
     @Override
     public void updatePos(int dt) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updatePos'");
+        if(this.pos.getY()>=370 && !(lastInput.isMoveUp())){
+            this.velocity= new Vector2d(0, 0);
+            this.setPos(new P2d(pos.getX(),370));
+        }else{
+            this.velocity= this.velocity.sum(vectorMouvement.mul(0.001*dt));
+            //add the gravity 
+            this.velocity= this.velocity.sum(gravity.mul(0.001*dt));
+            System.out.println(velocity);
+            this.setPos(pos.sum(this.velocity.mul(0.001*dt)));
+        }
     }
     
 }
