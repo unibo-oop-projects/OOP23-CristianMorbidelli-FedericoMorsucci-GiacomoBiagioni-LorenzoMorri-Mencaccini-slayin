e22b12a2ef36735455e5ca9c544d2ab33786ce2a
Giacomo Biagioni <giacomo.biagioni@studio.unibo.it>
2024-04-10 14:03:10 +0200

    moved gameloop into engine class

--- src/main/java/slayin/core/Engine.java
+++ src/main/java/slayin/core/Engine.java
@@ -1,10 +1,36 @@
 package slayin.core;
 
 public class Engine {
+    private long tickTime = 25; /* 40 fps */
 
-    public boolean stopLoop(){
-		/* TODO: check delle condizioni per terminare la partita */
-		return true;
-	}
-    
+    public void startGameLoop() {
+        long startTime, timePassed;
+
+        while (this.stopLoop()) { /* Game loop */
+            startTime = System.currentTimeMillis();
+
+            /* TODO: check input */
+
+            /* TODO: update game status */
+
+            /* TODO: render updates */
+
+            timePassed = System.currentTimeMillis() - startTime;
+            if (timePassed < tickTime) { /* wait until tickTime before nextFrame */
+                try {
+                    Thread.sleep(tickTime - timePassed);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            /* System.out.println(System.currentTimeMillis() - startTime); */
+        }
+    }
+
+    public boolean stopLoop() {
+        /* TODO: check delle condizioni per terminare la partita */
+        return true;
+    }
+
 }
