da6e2141980e0ca1e70673ef242d05f6df6612c8
Cristian Morbidelli <cristian.morbidelli@studio.unibo.it>
2024-04-12 22:31:10 +0200

    added an EntityFactory class to the utilities; it will be used in the future to create instances of GameObjects

---
+++ src/main/java/slayin/model/utility/EntityFactory.java
@@ -0,0 +1,9 @@
+package slayin.model.utility;
+
+/**
+ * A class that generates the objects of the entities that need to be added to the scene.
+ * In this class are described how different entities are created (starting positions, bounding boxes, etc...)
+ */
+public class EntityFactory {
+    // TODO
+}
