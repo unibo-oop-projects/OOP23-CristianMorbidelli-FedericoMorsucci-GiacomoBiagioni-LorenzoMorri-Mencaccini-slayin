05b2433ba390f9e4b324aaa4e4518380f20db762
Cristian Morbidelli <cristian.morbidelli@studio.unibo.it>
2024-04-12 22:24:54 +0200

    added a Dummy entity for tests purpose

---
+++ src/main/java/slayin/model/entities/Dummy.java
@@ -0,0 +1,31 @@
+package slayin.model.entities;
+
+import slayin.model.World;
+import slayin.model.bounding.BoundingBox;
+import slayin.model.movement.MovementController;
+import slayin.model.utility.P2d;
+import slayin.model.utility.Vector2d;
+
+/**
+ * A class representing a concrete implementation of a basic GameObject.
+ * It is a simple entity that does not move, doesn't deal damage and can't be killed.
+ * Its purpose is solely to help in tests, so it won't actually appear in the actual game.
+ */
+public class Dummy extends GameObject {
+
+    public Dummy(P2d pos, Vector2d vectorMovement, BoundingBox boundingBox, World world) {
+        super(pos, vectorMovement, boundingBox, world);
+    }
+
+    @Override
+    public void updateVel(MovementController input) {
+        //
+    }
+
+    @Override
+    public void updatePos(int dt) {
+        // the dummy has no movement
+        // TODO: add the possibility to make the dummy fall to the ground, if placed mid air?
+    }
+    
+}
