ed8904fbe5a6843fdc646164abc94f84d3370cf0
Cristian Morbidelli <cristian.morbidelli@studio.unibo.it>
2024-04-13 17:04:48 +0200

    added EntityFactory to build GameObject instances; moved the configs folder to correct position

--- src/main/java/slayin/model/utility/EntityFactory.java
+++ src/main/java/slayin/model/utility/EntityFactory.java
@@ -1,9 +1,37 @@
 package slayin.model.utility;
 
+import java.util.Random;
+
+import slayin.model.World;
+import slayin.model.bounding.BoundingBoxImplRet;
+import slayin.model.entities.GameObject;
+import slayin.model.entities.Dummy;
+
 /**
  * A class that generates the objects of the entities that need to be added to the scene.
  * In this class are described how different entities are created (starting positions, bounding boxes, etc...)
  */
 public class EntityFactory {
     // TODO
+    private final World world;
+    private final Random rn;
+
+    public EntityFactory(World w){
+        this.world = w;
+        rn = new Random();
+    }
+
+    public GameObject buildDummy(){
+        final Vector2d DUMMY_STARTING_MOVEMENT = new Vector2d(0, 0);  // Starts with 0 speed
+
+        final int DUMMY_HEIGHT = world.getHeight() / 20;
+        final int DUMMY_LENGHT = DUMMY_HEIGHT;
+
+        final int DUMMY_STARTING_X = (int) rn.nextInt(world.getWidth());    // Starts at a completely random X
+        final int DUMMY_STARTING_Y = world.getGround() - DUMMY_HEIGHT/2;    // Starts at the ground level
+
+        Dummy entity = new Dummy(new P2d(DUMMY_STARTING_X, DUMMY_STARTING_Y), DUMMY_STARTING_MOVEMENT, new BoundingBoxImplRet(new P2d(DUMMY_STARTING_X, DUMMY_STARTING_Y), DUMMY_LENGHT, DUMMY_HEIGHT), world);
+
+        return entity;
+    }
 }
--- src/test/java/levels/TestLevelFactory.java
+++ src/test/java/levels/TestLevelFactory.java
@@ -1,30 +1,48 @@
 package levels;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import slayin.model.Level;
+import slayin.model.World;
+import slayin.model.entities.Dummy;
 import slayin.model.entities.GameObject;
 import slayin.model.utility.LevelFactory;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 
 public class TestLevelFactory {
 
+    LevelFactory levelFactory;
+    final int worldWidth = 1000;
+    final int worldHeight = 1000;
+    final int groundLevel = 800;
+
+    @BeforeEach
+    void setUp(){
+        this.levelFactory = new LevelFactory(new World(worldWidth, worldHeight, groundLevel));
+    }
+
     @Test
     void testGetEnemies(){
-        Level test = LevelFactory.buildLevel(1);
+        Level test = levelFactory.buildLevel(0);
 
         List<GameObject> levelEnemies = new ArrayList<>();
-        GameObject obj;
+        Optional<GameObject> obj;
         do{
-            obj = test.dispatchEnemy().get();
-            if(obj!=null){
-                levelEnemies.add(obj);
+            obj = test.dispatchEnemy();
+            if(obj.isPresent()){
+                levelEnemies.add(obj.get());
             }
-        }while(obj!=null);
+        }while(obj.isPresent());
 
-        System.out.println(levelEnemies);
+        assertTrue(levelEnemies.size() == 1);
+        assertTrue(levelEnemies.get(0) instanceof Dummy);
     }
     
 }
