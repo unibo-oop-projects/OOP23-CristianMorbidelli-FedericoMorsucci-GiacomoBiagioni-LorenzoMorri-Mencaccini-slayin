8b1588351a438de4015a0359961c425dba184df3
Federico Morsucci <federico.morsucci@studio.unibo.it>
2024-04-15 17:06:03 +0200

    adding comments and lambdas to DrawComponentFactory

--- src/main/java/slayin/model/entities/graphics/DrawComponentFactory.java
+++ src/main/java/slayin/model/entities/graphics/DrawComponentFactory.java
@@ -13,24 +13,28 @@ import slayin.model.bounding.BoundingBoxImplCirc;
 import slayin.model.bounding.BoundingBoxImplRet;
 import slayin.model.entities.character.Character;
 import slayin.model.entities.character.Character.Direction;
-
+/**
+ * A class that generates the DrawComponent to draw objects.
+ */
 public class DrawComponentFactory {
 
     private final static String FORMAT_SPRITE=".png";
-    
-    public static DrawComponent graphicsComponentCharacter(Character character){
-        return new DrawComponent() {
 
-            @Override
-            public void draw(Graphics g) {
+    /**
+     * constructs a drawcomponent to draw a character
+     * @param character - character to draw
+     * @return DrawComponent that draws the character passed by parameter
+     */
+    public static DrawComponent graphicsComponentCharacter(Character character){
+        return (g)->{
                 URL pathKnight;
                 Image img;
                 // cerco la x più piccola per disegnare correttaemente il personaggio
                 double minX;
                 if(character.getDir()==Direction.LEFT){
-                    pathKnight = this.getClass().getResource("/assets/character/"+character.getClass().getSimpleName()+"Left"+FORMAT_SPRITE);
+                    pathKnight = DrawComponentFactory.class.getResource("/assets/character/"+character.getClass().getSimpleName()+"Left"+FORMAT_SPRITE);
                 }else{
-                    pathKnight = this.getClass().getResource("/assets/character/"+character.getClass().getSimpleName()+"Right"+FORMAT_SPRITE);
+                    pathKnight = DrawComponentFactory.class.getResource("/assets/character/"+character.getClass().getSimpleName()+"Right"+FORMAT_SPRITE);
                 }
                 try {
                     img = ImageIO.read(new File(pathKnight.toURI()));
@@ -52,18 +56,17 @@ public class DrawComponentFactory {
                     System.out.println("impossibile caricare l'immagine del personaggio");
                     e.printStackTrace();
                 }
-            }
-            
         };
 
     }
 
-
+    /**
+     * constructs a drawcomponent to draw a bounding box
+     * @param bBox - bounding box to draw
+     * @return DrawComponent that draws the boundind box passed by parameter
+     */
     public static DrawComponent graphicsComponentBoundigBox(BoundingBox bBox){
-        return new DrawComponent() {
-
-            @Override
-            public void draw(Graphics g) {
+        return (g)->{
                 if(bBox instanceof BoundingBoxImplRet){
                     BoundingBoxImplRet newBBox= (BoundingBoxImplRet)bBox;
                     g.drawRect((int)newBBox.getX(),(int)newBBox.getY(), (int)newBBox.getWidth(),(int) newBBox.getHeight());
@@ -72,8 +75,6 @@ public class DrawComponentFactory {
                     BoundingBoxImplCirc newBBox= (BoundingBoxImplCirc) bBox;
                     g.drawOval((int)newBBox.getPoint().getX(),(int)newBBox.getPoint().getY(), (int)newBBox.getRadius(),(int) newBBox.getRadius());
                 }
-            }
-            
         };
 
     }
