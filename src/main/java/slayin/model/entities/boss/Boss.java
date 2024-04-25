8d41e8c6b6230aa501b3cb774e01f16de3e5a6b5
Lorenzo Morri <lorenzo.morri6@studio.unibo.it>
2024-04-25 09:32:28 +0200

    added health management in "Boss.java" and "Minotaur.java"

--- src/main/java/slayin/model/entities/boss/Boss.java
+++ src/main/java/slayin/model/entities/boss/Boss.java
@@ -1,5 +1,68 @@
 package slayin.model.entities.boss;
 
-public interface Boss {
-    //TODO: implement method signature
+import slayin.model.World;
+import slayin.model.bounding.BoundingBox;
+import slayin.model.entities.GameObject;
+import slayin.model.utility.P2d;
+import slayin.model.utility.Vector2d;
+
+public class Boss extends GameObject {
+
+    private int health;
+    
+    
+    /**
+     * generic constructor
+     * @param pos
+     * @param vectorMovement
+     * @param boundingBox
+     * @param world
+     * 
+     */
+    public Boss(P2d pos, Vector2d vectorMovement, BoundingBox boundingBox, World world) {
+        super(pos, vectorMovement, boundingBox, world);
+    }
+
+
+    @Override
+    public void updatePos(int dt) {
+        // every boss has a specific one
+    }
+
+    
+    /**
+     * @return boss life
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * set boss life
+     * @param health 
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    /**
+     * set boss life - n if n and health is greater then 0
+     * @param n - damage
+     */
+    public void changeHealth(int n){
+        if(n>0 && this.health>0){
+            this.health = (this.health-n);
+        }
+    }
+
+    /**
+     * @return true if health is less or equal then 0
+     */
+    public boolean idDead(){
+        boolean outcome=false;
+        if(this.health<=0){
+            outcome=true;
+        }
+        return outcome;
+    }
 }
--- src/main/java/slayin/model/entities/boss/Minotaur.java
+++ src/main/java/slayin/model/entities/boss/Minotaur.java
@@ -2,16 +2,21 @@ package slayin.model.entities.boss;
 
 import slayin.model.World;
 import slayin.model.bounding.BoundingBox;
-import slayin.model.entities.GameObject;
 import slayin.model.utility.P2d;
 import slayin.model.utility.Vector2d;
 
-public class Minotaur extends GameObject implements Boss  {
-
-    //TODO: initialize boss position & velocity
+public class Minotaur extends Boss  {
+    /**
+     * Minotaur constructor, set initial health
+     * @param pos
+     * @param vectorMovement
+     * @param boundingBox
+     * @param world
+     * 
+     */
     public Minotaur(P2d pos, Vector2d vectorMovement, BoundingBox boundingBox, World world) {
         super(pos, vectorMovement, boundingBox, world);
-        //TODO Auto-generated constructor stub
+        this.setHealth(5); //The Minotaur must receive 5 hits to be defeated
     }
 
     @Override
