f261c575dd03e339b7240bd62dd2e076e9d6cb2f
Mencaccini <s_mncmtt03r05l500b@itisurbino.it>
2024-06-23 16:43:19 +0200

    implemented coualt movement, now he randomly go down, wait, and come up

--- src/main/java/slayin/model/entities/enemies/Couatl.java
+++ src/main/java/slayin/model/entities/enemies/Couatl.java
@@ -16,26 +16,29 @@ public class Couatl extends Enemy{
     private static final int scorePerKill = 3;
     private static final int damageOnHit = 2;
     private int oldDt = 0;
-    private Boolean pause = false,down = false,up = false;
     private Random random;
     private int SPEEDX = this.getWorld().getWidth()/13;
-    private int SLOWSPEEDY = this.getWorld().getWidth()/4;
+    private int SLOWSPEEDY = -this.getWorld().getWidth()/4;
     private int FASTSPEEDY = this.getWorld().getHeight()/2;
     private double startingY;
+    private static enum State {PAUSE,DOWN,UP,FREE}
+    private State state,oldState;
 
     public Couatl(P2d pos, BoundingBox boundingBox, World world, GameEventListener eventListener) {
         super(pos, new Vector2d(0, 0), boundingBox, world, eventListener);
         random = new Random();
         startingY = this.getPos().getY();
+        this.state = State.FREE;
+        this.oldState = State.FREE;
     }
+
+    
 
     @Override
     public void updatePos(int dt){
         oldDt+=dt;
         if(oldDt>2000){
-            if(!down && !up){
-                pause = !pause;
-            }
+            this.changeState();
             oldDt = 0;
         } 
         this.updateDir();
@@ -44,10 +47,29 @@ public class Couatl extends Enemy{
         // aggiorno di nuovo la BoundinBox
         this.getBoundingBox().updatePoint(this.getPos());
     }
+
+    private void changeState(){
+        if(this.state==State.FREE){
+            this.oldState=State.FREE;
+            this.state=State.PAUSE;
+        }else if(this.oldState==State.FREE && this.state==State.PAUSE){
+            this.oldState=State.PAUSE;
+            this.state=State.DOWN;
+        }else if(this.oldState==State.PAUSE && this.state==State.DOWN){
+            this.oldState=State.DOWN;
+            this.state=State.PAUSE;
+        }else if(this.oldState==State.DOWN && this.state==State.PAUSE){
+            this.oldState=State.PAUSE;
+            this.state=State.UP;
+        }else if(this.oldState==State.PAUSE && this.state==State.UP){
+            this.oldState=State.UP;
+            this.state=State.FREE;
+        }
+    }
     
     @Override
     public void updateDir(){
-        if(this.getVectorMovement().equals(new Vector2d(0, 0)) && !pause && this.getPos().getY()==startingY){
+        if(this.state==State.FREE && (this.getVectorMovement().equals(new Vector2d(0, 0)) || this.getVectorMovement().equals(new Vector2d(0, SLOWSPEEDY)))){
             if(random.nextInt(2)==1){
                 setDir(Direction.LEFT);
                 this.setVectorMovement(new Vector2d(-SPEEDX, 0)); 
@@ -56,24 +78,21 @@ public class Couatl extends Enemy{
                 this.setVectorMovement(new Vector2d(SPEEDX, 0));
             }
         }else{
-            if(pause && !down && !up){
+            if(this.state==State.PAUSE){
                 this.setVectorMovement(new Vector2d(0, 0));
-            }else if(up){
+            }else if(this.state==State.UP){
                 this.setVectorMovement(new Vector2d(0, SLOWSPEEDY));
                 if(this.getPos().getY()<=startingY){
-                    up = false;
+                    changeState();
                 }
-            }else if(down){
+            }else if(this.state==State.DOWN){
                 this.setVectorMovement(new Vector2d(0, FASTSPEEDY));
                 if(this.getWorld().isTouchingGround(this)){
-                    down = false;
-                    pause = true;
+                    changeState();
                 }
             }
         }
         
-        
-        /////
         var collision = this.getWorld().collidingWith(this);
         for(var col : collision){
 
