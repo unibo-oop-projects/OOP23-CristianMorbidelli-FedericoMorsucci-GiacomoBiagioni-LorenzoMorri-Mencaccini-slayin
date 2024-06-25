c00d68c2402dc20beaf4efea2d9c23f6ff416b11
Mencaccini <s_mncmtt03r05l500b@itisurbino.it>
2024-06-25 16:29:12 +0200

    added comments

--- src/main/java/slayin/model/utility/EntityFactory.java
+++ src/main/java/slayin/model/utility/EntityFactory.java
@@ -44,6 +44,12 @@ public class EntityFactory {
         return entity;
     }
 
+    /**
+     * Creates and returns a new Slime enemy.
+     *
+     * @return a new Slime enemy.
+     */
+
     public Enemy buildSlime(){
 
         final int SLIME_HEIGHT = world.getHeight() / 18;
@@ -57,12 +63,18 @@ public class EntityFactory {
         return entity;
     }
 
+    /**
+     * Creates and returns a new Fire enemy.
+     *
+     * @return a new Fire enemy.
+     */
+
     public Enemy buildFire(){
         final int FIRE_HEIGHT = world.getHeight() / 14;
         final int FIRE_LENGHT = FIRE_HEIGHT;
 
         final int FIRE_STARTING_X = (int) rn.nextInt(world.getWidth());    // Starts at a completely random X
-        final int FIRE_STARTING_Y = world.getHeight()/2 - FIRE_HEIGHT/2;    // Starts at the ground level
+        final int FIRE_STARTING_Y = world.getHeight()/2 - FIRE_HEIGHT/2;    // Starts at half the world height
 
         Fire entity = new Fire(new P2d(FIRE_STARTING_X, FIRE_STARTING_Y),  new BoundingBoxImplRet(new P2d(FIRE_STARTING_X, FIRE_STARTING_Y), FIRE_LENGHT, FIRE_HEIGHT), world, this.eventListener);
 
@@ -81,31 +93,42 @@ public class EntityFactory {
         return new Imp(null, boundingBox, this.world, this.eventListener);
     }
 
+    /**
+     * Creates and returns a new Couatl enemy.
+     *
+     * @return a new Couatl enemy.
+     */
 
     public Enemy buildCouatl(){
         final int COUATL_HEIGHT = world.getHeight() / 12;
         final int COUATL_LENGHT = COUATL_HEIGHT;
 
-        final int DUMMY_STARTING_X = (int) rn.nextInt(world.getWidth());    // Starts at a completely random X
-        final int DUMMY_STARTING_Y = world.getHeight()/2 - COUATL_HEIGHT/2;    // Starts at the ground level
+        final int COUATL_STARTING_X = (int) rn.nextInt(world.getWidth());    // Starts at a completely random X
+        final int COUATL_STARTING_Y = world.getHeight()/2 - COUATL_HEIGHT/2;    // Starts at half the world height
 
-        Couatl entity = new Couatl(new P2d(DUMMY_STARTING_X, DUMMY_STARTING_Y),  new BoundingBoxImplRet(new P2d(DUMMY_STARTING_X, DUMMY_STARTING_Y), COUATL_LENGHT, COUATL_HEIGHT), world, this.eventListener);
+        Couatl entity = new Couatl(new P2d(COUATL_STARTING_X, COUATL_STARTING_Y),  new BoundingBoxImplRet(new P2d(COUATL_STARTING_X, COUATL_STARTING_Y), COUATL_LENGHT, COUATL_HEIGHT), world, this.eventListener);
 
         return entity;
     }    
+
+    /**
+     * Creates and returns a new Headstone enemy.
+     *
+     * @return a new Headstone enemy.
+     */
 
     public Enemy buildHeadstone(){
         final int HEADSTONE_HEIGHT = world.getHeight() / 14;
         final int HEADSTONE_LENGHT = HEADSTONE_HEIGHT;
         int spawnx;
 
-        if(rn.nextInt(1)==1){
+        if(rn.nextInt(2)==1){
             spawnx = world.getWidth()-HEADSTONE_LENGHT/2;
         }else{
             spawnx = HEADSTONE_LENGHT/2;
         }
 
-        final int HEADSTONE_STARTING_X = spawnx;    // Starts at a completely random X
+        final int HEADSTONE_STARTING_X = spawnx;    // Starts at full left of the screen or full right
         final int HEADSTONE_STARTING_Y = world.getGround()-HEADSTONE_HEIGHT/2;    // Starts at the ground level
 
         Headstone entity = new Headstone(new P2d(HEADSTONE_STARTING_X, HEADSTONE_STARTING_Y),  new BoundingBoxImplRet(new P2d(HEADSTONE_STARTING_X, HEADSTONE_STARTING_Y), HEADSTONE_LENGHT, HEADSTONE_HEIGHT), world, this.eventListener);
